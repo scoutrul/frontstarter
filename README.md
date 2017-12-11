6d5b4440dd0cacf903ad19d6a2dc405121dce430
Anton <scoutrul@mail.ru>
2017-12-11 15:04:49 +0300

    Readme, Scroll fix

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
 # Static
-GitHub Pages link: [https://scoutrul.github.io/frontstart/](https://scoutrul.github.io/frontstart/)
-## Technologies
-*  responsive web app
-* [Sass](http://sass-lang.com/), [PostCSS](http://postcss.org/) and [Autoprefixer](https://github.com/postcss/autoprefixer) for styles processing
+link: [https://frontstarter.ru/](https://frontstarter.ru)
+
+## Used technologies
+
+Viewing:
+* SPA,
+* Adaptive design,
+* Mobile application,
+* Responsive text size,
+* CSSAnimation,
+* Minimalism
+
+Hosting:
+* SSL, .ru, 
+* Firebase, gh-pages,
+* Service Workers Ready
 
 
-## Preinstalled and configured libraries
-* [react@16](https://github.com/facebook/react)
-* [redux](https://github.com/reactjs/redux)
-* [react-router@4](https://github.com/ReactTraining/react-router) with HashRouter (perfectly suits Github Pages environment)
-* [react-redux](https://github.com/reactjs/react-redux)
-* [axios](https://github.com/mzabriskie/axios)
-* [ramda](http://ramdajs.com/) and [lodash](https://lodash.com/) as an utility libraries (unused parts will be cut off with babel and webpack plugins)
+Production:
+* WebStrom
+* NPM, WebPack,
+* SCSS, AutoPrefixer,
+* GIT,
+* Optimized deploying
+
+Environment:
+* ReactJS v16,
+* Redux-act,
+* Router v4,
+* ES6, Babel,
+* Eslint
+
+
--- src/components/routes/about/About.js
+++ src/components/routes/about/About.js
@@ -6,23 +6,47 @@ export default () =>
 	<section>
 		<div className='contentView'>
 			<main>
-				<h1>Hello! My name is Anton</h1>
-				<div>I'm looking for a frontend-developer job position in Moscow.</div>
-				<div>My favorite framework is ReactJS.</div>
-				<div>I have a great experience with buildinw PWA with
-					<br/>flexibility design for desktop and mobile platforms.
-				</div>
-				<h2>Basic skills</h2>
+				<h1>This site</h1>
+				link: https://frontstarter.ru
 				
+				<h2>Used technologies</h2>
+
+				<h3>Viewing:</h3>
 				<ul>
-					<li>JavaScript (ES6+)</li>
-					<li>React & Redux</li>
-					<li>HTML5, CSS3</li>
+					<li>SPA</li>
+					<li>Adaptive design</li>
+					<li>Mobile application</li>
+					<li>Responsive text size</li>
+					<li>CSSAnimation</li>
+					<li>Minimalism</li>
 				</ul>
+
+				<h3>Hosting:</h3>
+				<ul>
+					<li>SSL, .ru,</li>
+					<li>Firebase, gh-pages,</li>
+					<li>Service Workers Ready</li>
+				</ul>
+				
+				<h3>Production:</h3>
+				<ul>
+					<li>WebStrom</li>
+					<li>NPM, WebPack</li>
+					<li>SCSS, AutoPrefixer</li>
+					<li>GIT</li>
+					<li>Optimized deploying</li>
+				</ul>
+
+				<h3>Environment:</h3>
+				<ul>
+					<li>ReactJS v16</li>
+					<li>Redux-act</li>
+					<li>Router v4</li>
+					<li>ES6, Babel</li>
+					<li>Eslint</li>
+				</ul>
+				
 			</main>
-		</div>
-		<div className="links">
-			<Link to="/works">Works</Link>
 		</div>
 	</section>
 
