2d053ba647cde43cdff17ffda5b02f656cdafe42
Anton <scoutrul@mail.ru>
2018-10-11 16:30:26 +0300

    home effect lol

--- src/components/pages/home/HomePage.jsx
+++ src/components/pages/home/HomePage.jsx
@@ -1,51 +1,53 @@
 import React, {Component} from 'react'
 import './home.scss'
 
-
-// надо что бы от мыши была дистанция по радиусу от центра элемента
-// от этой цифры будет тень гулять цсс
-
 class Home extends Component {
-
-	calculateDistance = (elem, mouseX, mouseY, ofsX, ofsY) => {
-		console.log(elem);
-        return Math.floor(
-			Math.sqrt(
-				Math.pow(mouseX - (elem.offset().left+(elem.width()/2)), 2) + Math.pow(mouseY - (elem.offset().top+(elem.height()/2)), 2)
-			)
-		);
+	state = {
+		mX: null,
+		mY: null,
 	}
 
-	onMouseListener = (e, el) => {
-		console.log(e);
+	onMouseListener = (e) => {
 		const mX = e.x;
 		const mY = e.y;
-		const ofsX = e.offsetX;
-		const ofsY = e.offsetY;
-		const distance = this.calculateDistance(el, mX, mY, ofsX, ofsY);
-		console.log(distance);    
+
+		this.setState({
+			mX,
+			mY
+		});
+		
 	}
 
 	componentDidMount(){
 		console.log('hi');
 
-		this.home.addEventListener('mouseover', (e) => this.onMouseListener(e, this.frontend));
+		this.view.addEventListener('mouseover', this.onMouseListener);
 	}
 
 	componentWillUnmount(){
 		console.log('bb');
 
-		this.home.removeEventListener('mouseover', (e) => this.onMouseListener(e, this.frontend));
+		this.view.removeEventListener('mouseover', this.onMouseListener);
 	}
 
 	render(){
+		const shadow = this.state.mY / 1000;
+		const frontend = {
+			'textShadow' : `-0.5rem 0.4rem ${shadow}rem rgba(28, 17, 53, 0.6)`,
+			'top' : -this.state.mX/100,
+			'left' : this.state.mY/100,
+		}
+		const developer = {
+			'top' : this.state.mX/100,
+			'left' : -this.state.mY/100,
+		}
 		return(
-			<section className='home' ref={elem => this.home = elem}>
-				<div className='contentView' >
+			<section className='home' >
+				<div className='contentView' ref={elem => this.view = elem} >
 					<h1>
-						<span className='high' ref={elem => this.frontend = elem}>FrontEnd</span><br/>
+						<span className='high' style={frontend}>FrontEnd</span><br/>
 						
-						<span className='low' id='developer'>Moscow developer</span>
+						<span className='low' id='developer' style={developer}>Moscow developer</span>
 					</h1>
 				</div>
 			</section>
